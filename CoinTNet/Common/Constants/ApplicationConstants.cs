c66bab96b2b3eda2bffa0e9c240b13a03b4e6d93
wishhhmaster <wishhhmaster@outlook.com>
2014-05-20 16:26:55 +0200

    Incremented version number

--- CoinTNet/Common/Constants/ApplicationConstants.cs
+++ CoinTNet/Common/Constants/ApplicationConstants.cs
@@ -6,17 +6,41 @@ using System.Threading.Tasks;
 
 namespace CoinTNet.Common.Constants
 {
+    /// <summary>
+    /// General constants about the application
+    /// </summary>
     class ApplicationConstants
     {
+        /// <summary>
+        /// The application's name
+        /// </summary>
         public const string ApplicationName = "CoinT.Net";
-        public const string CurrentVersion = "0.0.0.4";
+        /// <summary>
+        /// The application's current version
+        /// </summary>
+        public const string CurrentVersion = "0.0.0.5";
     }
 
+    /// <summary>
+    /// Keys for the encrypted data
+    /// </summary>
     class SecuredDataKeys
     {
+        /// <summary>
+        /// Key for Bitstamp's API keys
+        /// </summary>
         public const string BitstampAPI = "bitstampAPI";
+        /// <summary>
+        /// Key for Btc-e's API keys
+        /// </summary>
         public const string BtceAPI = "btceAPI";
+        /// <summary>
+        /// Key for Twitter's API keys
+        /// </summary>
         public const string TwitterAPI = "twitterAPI";
+        /// <summary>
+        /// Key for Cryptsy's API keys
+        /// </summary>
         public const string CryptsyAPI = "cryptsyAPI";
     }
 }
